7cd701854c5ff05d51bc88425a9ce7e40856ac6b
thePositiveReinforcer <thePositiveReinforcer@protonmail.com>
2021-09-17 12:15:06 +0530

    Base class setup.

---
+++ src/main/java/com/pinch/org/login/LoginHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.pinch.org.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinch/org/login/LoginHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinch.org.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
